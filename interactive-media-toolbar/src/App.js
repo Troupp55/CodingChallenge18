ee408890f6fad41bd354f909d2c05ab48e4be26a
Tristen Roupp <tristenr@usf.edu>
2024-07-15 22:32:22 -0400

    Updated App js file

--- interactive-media-toolbar/src/App.js
+++ interactive-media-toolbar/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import RateMovieButton from './RateMovieButton';
+import MovieSelector from './MovieSelector';
+import UploadButton from './UploadButton';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [selectedMovie, setSelectedMovie] = useState('Movie 1');
+  const movies = ['Movie 1', 'Movie 2', 'Movie 3'];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Interactive Media Toolbar</h1>
+        <div className="toolbar">
+          <MovieSelector movies={movies} onSelect={setSelectedMovie} />
+          <RateMovieButton movie={selectedMovie} />
+          <UploadButton />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
